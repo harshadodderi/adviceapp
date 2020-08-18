971f110b7e4b86b25dfdc2aa7a96c6f4ca847c02
imnotharsha <harshadodderi@gmail.com>
2020-08-18 22:21:30 +0530

    Updates

---
+++ precache-manifest.020e2b6fddf7d1820a12726d0687d720.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac705ff69d4c0d899232d6d6233357a",
+    "url": "/adviceapp/index.html"
+  },
+  {
+    "revision": "c02d7ade33e2f333a0b4",
+    "url": "/adviceapp/static/css/main.3cba44b5.chunk.css"
+  },
+  {
+    "revision": "6d8285553921b694c0d0",
+    "url": "/adviceapp/static/js/2.9b3ec133.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adviceapp/static/js/2.9b3ec133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c02d7ade33e2f333a0b4",
+    "url": "/adviceapp/static/js/main.4d74fa73.chunk.js"
+  },
+  {
+    "revision": "5302220baadd3d5fb892",
+    "url": "/adviceapp/static/js/runtime-main.624bc284.js"
+  },
+  {
+    "revision": "841b1685dc4cea8c2838f91e3a73404e",
+    "url": "/adviceapp/static/media/nature.841b1685.jpg"
+  }
+]);
